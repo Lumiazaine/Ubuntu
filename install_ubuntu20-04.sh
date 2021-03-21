8d8a6b1eecc2a1f8d0b415e1beb4bc0c01bafb47
Lumiazaine <lumiazaine@gmail.com>
2021-03-21 13:42:34 +0100

    modificacion de estructura

--- install_ubuntu20-04.sh
+++ install_ubuntu20-04.sh
@@ -1,21 +1,33 @@
+# Descarga paquetes
+wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
+wget https://github.com/martpie/museeks/releases/download/0.11.4/museeks-amd64.deb
+wget https://github.com/Ulauncher/Ulauncher/releases/download/5.10.0/ulauncher_5.10.0_all.deb
+
+# Actualización de repositorio y sistema
+
 sudo apt update && sudo apt upgrade -y
+
+# Descarga apt 
+
 sudo apt install -y curl guake neofetch grub-customizer indicator-cpufreq 
+sudo apt install -y gparted gnome-shell-extension-prefs gnome-tweaks nodejs npm git spotify-client
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt update
-sudo apt install -y gparted gnome-shell-extension-prefs gnome-tweaks nodejs npm git spotify-client
+sudo apt install -f
+
+# Instalación paquetes
+
+sudo apt install -y ./museeks-amd64.deb
+sudo apt install -y./ulauncher_5.10.0_all.deb
+sudo apt install -y ./code_1.54.3-1615806378_amd64.deb
+
+# Instalación Snaps
+
 sudo snap install slack --classic
 sudo snap install telegram-desktop
 sudo snap install insomnia
-wget https://github.com/martpie/museeks/releases/download/0.11.4/museeks-amd64.deb
-sudo apt install -y ./museeks-amd64.deb
-wget https://github.com/Ulauncher/Ulauncher/releases/download/5.10.0/ulauncher_5.10.0_all.deb
-sudo apt install ./ulauncher_5.10.0_all.deb
-sudo apt-get update
-wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
-sudo apt install -y ./code_1.54.3-1615806378_amd64.deb
-sudo apt install -f
-# Style and customizationc
+
+# Style and customization
 
 tar -xf ./Mojave-dark.tar.xz
 mkdir ~/.themes
@@ -26,4 +38,3 @@ gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 22
-
