58f5698c705c785b66cbaf71abc7a63b67d6000f
Lumiazaine <lumiazaine@gmail.com>
2021-03-21 13:54:55 +0100

    modificaciones en script

--- install_ubuntu20-04.sh
+++ install_ubuntu20-04.sh
@@ -1,3 +1,20 @@
+OS=$(awk '/DISTRIB_ID=/' /etc/*-release | sed 's/DISTRIB_ID=//' | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
+VERSION=$(awk '/DISTRIB_RELEASE=/' /etc/*-release | sed 's/DISTRIB_RELEASE=//' | sed 's/[.]0/./')
+
+if [ -z "$OS" ]; then
+    OS=$(awk '{print $1}' /etc/*-release | tr '[:upper:]' '[:lower:]')
+fi
+
+if [ -z "$VERSION" ]; then
+    VERSION=$(awk '{print $3}' /etc/*-release)
+fi
+
+echo Hello, your system is $OS $VERSION $ARCH bits
+
+
+
+
 # Descarga paquetes
 wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
 wget https://github.com/martpie/museeks/releases/download/0.11.4/museeks-amd64.deb
