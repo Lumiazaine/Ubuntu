1dcca3bd374b98b80ceb3e15afeae67a816bd652
Lumiazaine <lumiazaine@gmail.com>
2021-03-21 13:27:02 +0100

    modificaciones snap

--- install_ubuntu20-04.sh
+++ install_ubuntu20-04.sh
@@ -6,21 +6,16 @@ sudo apt update
 sudo apt install -y gparted gnome-shell-extension-prefs gnome-tweaks nodejs npm git spotify-client
 sudo snap install slack --classic
 sudo snap install telegram-desktop
+sudo snap install insomnia
 wget https://github.com/martpie/museeks/releases/download/0.11.4/museeks-amd64.deb
-sudo dpkg -i ./museeks-amd64.deb
-sudo apt install -f
+sudo apt install -y ./museeks-amd64.deb
 wget https://github.com/Ulauncher/Ulauncher/releases/download/5.10.0/ulauncher_5.10.0_all.deb
 sudo apt install ./ulauncher_5.10.0_all.deb
-echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" \
-    | sudo tee -a /etc/apt/sources.list.d/insomnia.list
-wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc \
-    | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install insomnia
 wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
-sudo apt install ./code_1.54.3-1615806378_amd64.deb
-
-# Style and customization
+sudo apt install -y ./code_1.54.3-1615806378_amd64.deb
+sudo apt install -f
+# Style and customizationc
 
 tar -xf ./Mojave-dark.tar.xz
 mkdir ~/.themes
